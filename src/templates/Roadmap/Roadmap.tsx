2ab04ef6317978c7dfbb8cdebf2c9c34d72c1b53
Marton Lederer <marton@lederer.hu>
2023-02-01 19:04:34 +0100

    fix: scroll roadmap section

--- src/templates/Roadmap/Roadmap.tsx
+++ src/templates/Roadmap/Roadmap.tsx
@@ -19,6 +19,7 @@ export function useHorizontalScroll() {
     const el = elRef.current as any
     if (el) {
       const onWheel = (e: any) => {
+        if (el.scrollWidth - el.clientWidth <= el.scrollLeft + e.deltaY) return
         if (e.deltaY == 0) return
         e.preventDefault()
         el.scrollTo({
@@ -41,13 +42,20 @@ const Roadmap: NextPage = () => {
     <>
       <NextSeo {...PAGE_SEO} />
       <NavbarDesktop />
-      <div className="h-[1232px] w-full bg-red-500"></div>
-      <div ref={scrollRef as any} className="w-full overflow-x-hidden">
-        <div className="h-96 w-[4000px] bg-neutral-100 border-red-100 text-[120px]">
-          Text should scroll Text should scroll Text should scroll Text should
-          scroll Text should scroll Text should scroll Text should scroll Text
-          should scroll
-        </div>
+      <div
+        ref={scrollRef as any}
+        className="w-screen overflow-x-hidden flex"
+        // screen height - nav height
+        style={{ height: 'calc(100vh - 90px)' }}
+      >
+        <div className="h-full w-screen shrink-0 bg-red-500"></div>
+        <div className="h-full w-screen shrink-0 bg-blue-500"></div>
+        <div className="h-full w-screen	shrink-0 bg-green-500"></div>
+      </div>
+      <div className="h-96 w-[4000px] bg-neutral-100 border-red-100 text-[120px]">
+        Text should scroll Text should scroll Text should scroll Text should
+        scroll Text should scroll Text should scroll Text should scroll Text
+        should scroll
       </div>
       WIP.
       <Footer />
