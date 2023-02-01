dbff118d23d43984d90da4a0cdfc07e23b8f40eb
TheHoltz <holtzs.william@gmail.com>
2023-02-01 14:10:15 -0300

    feat: :sparkles: roadmap horizontall scroll

--- src/templates/Roadmap/Roadmap.tsx
+++ src/templates/Roadmap/Roadmap.tsx
@@ -5,26 +5,52 @@ import type { NextPage } from 'next'
 import { NextSeo } from 'next-seo'
 
 const useRoadmap = () => {
-
   const PAGE_SEO = {
     title: 'Page Title',
-    description: 'Page Description',
+    description: 'Page Description'
   }
 
   return { PAGE_SEO }
 }
 
+export function useHorizontalScroll() {
+  const elRef = useRef()
+  useEffect(() => {
+    const el = elRef.current as any
+    if (el) {
+      const onWheel = (e: any) => {
+        if (e.deltaY == 0) return
+        e.preventDefault()
+        el.scrollTo({
+          left: el.scrollLeft + e.deltaY,
+          // behavior: 'smooth'
+        })
+      }
+      el.addEventListener('wheel', onWheel)
+      return () => el.removeEventListener('wheel', onWheel)
+    }
+  }, [])
+  return elRef
+}
+
 const Roadmap: NextPage = () => {
   const { PAGE_SEO } = useRoadmap()
+  const scrollRef = useHorizontalScroll()
 
   return (
     <>
       <NextSeo {...PAGE_SEO} />
       <NavbarDesktop />
-      {/* <div ref={scrollRef} style={{ overflow: "auto" }} className="h-96 w-[4000px] bg-black border-red-100">
-        <h2>The Roadmap</h2>
-        <p></p>
-      </div> */}
+      <div className="h-[1232px] w-full bg-red-500"></div>
+      <div
+        // @ts-ignore
+        ref={scrollRef}
+        className="h-96 w-[4000px] bg-neutral-100 border-red-100 text-[120px]"
+      >
+        Text should scroll Text should scroll Text should scroll Text should
+        scroll Text should scroll Text should scroll Text should scroll Text
+        should scroll
+      </div>
       WIP.
       <Footer />
     </>
