9db2abc74757efce46db9d3a63625c44f3a8e57f
TheHoltz <holtzs.william@gmail.com>
2023-02-20 17:40:09 -0300

    fix: :bug: gtag fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,11 @@ import '@/styles/globals.css'
 
 import type { AppProps } from 'next/app'
 import NextNProgress from 'nextjs-progressbar'
+import useGoogleTagManager from "@/hooks/useGoogleTagManager"
 
 const App = ({ Component, pageProps }: AppProps) => {
+  useGoogleTagManager()
+
   return (
     <>
       <NextNProgress color="#000" height={6} key="progress-bar" />
