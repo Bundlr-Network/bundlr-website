2243d59405bc8178301c8ff0db249bfd3cc777d2
TheHoltz <holtzs.william@gmail.com>
2023-02-21 22:41:38 -0300

    fix: :bug: gus feedbacks

--- src/components/SpotifySection/SpotifySection.tsx
+++ src/components/SpotifySection/SpotifySection.tsx
@@ -12,12 +12,12 @@ const SpotifySection = () => {
   return (
     <>
       <section className='px-5 lg:px-[79px]'>
-        <div className="bg-black flex items-center flex-col gap-6 justify-center text-white w-full py-16 rounded-2xl">
-          <SpotifyIcon className='hover:rotate-6 transition-all' />
-          <a href="https://open.spotify.com/episode/1hifGHFRubtXSwFPnDM0jK?si=bc955dd0411445ca" target={"_blank"} rel="noreferrer">
+        <a href="https://open.spotify.com/episode/1hifGHFRubtXSwFPnDM0jK?si=bc955dd0411445ca" target={"_blank"} rel="noreferrer">
+          <div className="bg-black flex items-center flex-col gap-6 justify-center text-white w-full py-16 rounded-2xl">
+            <SpotifyIcon className='hover:rotate-6 transition-all' />
             <p className='text-white font-robotoMono text-base text-center mt-auto hover:font-bold'>[  LISTEN TO OUR JOURNEY  ]</p>
-          </a>
-        </div>
+          </div>
+        </a>
       </section>
     </>
   )
